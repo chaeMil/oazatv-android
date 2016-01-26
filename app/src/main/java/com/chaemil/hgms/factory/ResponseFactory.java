16aaaff63c46d7a7b0193a4a70790cc72c5230dc
Michal Mlejnek <chaemil72@gmail.com>
2016-01-26 20:44:06 +0100

    added photos and photoalbum parsers

--- app/src/main/java/com/chaemil/hgms/factory/ResponseFactory.java
+++ app/src/main/java/com/chaemil/hgms/factory/ResponseFactory.java
@@ -2,9 +2,12 @@ package com.chaemil.hgms.factory;
 
 
 import com.chaemil.hgms.model.ArchiveItem;
+import com.chaemil.hgms.model.Photo;
+import com.chaemil.hgms.model.PhotoAlbum;
 import com.chaemil.hgms.model.Video;
 import com.chaemil.hgms.utils.Constants;
 import com.chaemil.hgms.utils.SmartLog;
+import com.google.gson.JsonObject;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,7 +21,7 @@ public class ResponseFactory {
     public static Video parseVideo(JSONObject response) {
 
         try {
-            if (response.getString(Constants.JSON_TYPE).equals(Constants.JSON_VIDEO)) {
+            if (response.getString(Constants.JSON_TYPE).equals(Constants.JSON_TYPE_VIDEO)) {
 
                 int serverId = response.getInt(Constants.JSON_ID);
                 String hash = response.getString(Constants.JSON_HASH);
@@ -43,6 +46,65 @@ public class ResponseFactory {
 
         return null;
 
+    }
+
+    public static PhotoAlbum parseAlbum(JSONObject response) {
+
+        try  {
+            if (response.getString(Constants.JSON_TYPE).equals(Constants.JSON_TYPE_ALBUM)) {
+
+                int serverId = response.getInt(Constants.JSON_ID);
+                String hash = response.getString(Constants.JSON_HASH);
+                String date = response.getString(Constants.JSON_DATE);
+                String nameCS = response.getString(Constants.JSON_NAME_CS);
+                String nameEN = response.getString(Constants.JSON_NAME_EN);
+                String tags = response.getString(Constants.JSON_TAGS);
+                String descriptionCS = response.getString(Constants.JSON_DESCRIPTION_CS);
+                String descriptionEN = response.getString(Constants.JSON_DESCRIPTION_EN);
+                JSONArray jsonPhotos = response.getJSONArray(Constants.JSON_PHOTOS);
+
+                ArrayList<Photo> photos = new ArrayList<>();
+
+                for(int c = 0; c < jsonPhotos.length(); c++) {
+
+                    Photo photo = parsePhoto(jsonPhotos.getJSONObject(c));
+                    photos.add(photo);
+
+                }
+
+                return new PhotoAlbum(serverId, hash, date, nameCS, nameEN, tags, descriptionCS,
+                        descriptionEN, photos);
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    public static Photo parsePhoto(JSONObject response) {
+
+        try {
+
+            String descriptionCS = response.getString(Constants.JSON_DESCRIPTION_CS);
+            String descriptionEN = response.getString(Constants.JSON_DESCRIPTION_EN);
+            String originalFile = response.getString(Constants.JSON_ORIGINAL_FILE);
+            String thumb128 = response.getString(Constants.JSON_THUMB + "_128");
+            String thumb256 = response.getString(Constants.JSON_THUMB + "_256");
+            String thumb512 = response.getString(Constants.JSON_THUMB + "_512");
+            String thumb1024 = response.getString(Constants.JSON_THUMB + "_1024");
+            String thumb2048 = response.getString(Constants.JSON_THUMB + "_2048");
+
+            return new Photo(originalFile, thumb128, thumb256, thumb512, thumb1024, thumb2048,
+                    descriptionCS, descriptionEN);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     public static ArrayList<ArchiveItem> parseArchive(JSONObject response) {
--- app/src/main/java/com/chaemil/hgms/utils/Constants.java
+++ app/src/main/java/com/chaemil/hgms/utils/Constants.java
@@ -59,4 +59,8 @@ public class Constants {
     public static final String JSON_TYPE = "type";
     public static final String JSON_TYPE_VIDEO = "video";
     public static final String JSON_TYPE_ALBUM = "album";
+    public static final String JSON_ORIGINAL_FILE = "original_file";
+    public static final String JSON_THUMB = "thumb";
+    public static final String JSON_ORDER = "order";
+    public static final String JSON_PHOTOS = "photos";
 }
