52c5fea21ff5bee76f509c7409cc39c66b057964
Michal Mlejnek <chaemil72@gmail.com>
2016-02-08 21:34:47 +0100

    added image placehodler bg added context emnu to archive

--- app/src/main/java/com/chaemil/hgms/adapter/ArchiveAdapter.java
+++ app/src/main/java/com/chaemil/hgms/adapter/ArchiveAdapter.java
@@ -1,11 +1,14 @@
 package com.chaemil.hgms.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 import com.chaemil.hgms.OazaApp;
 import com.chaemil.hgms.R;
 import com.chaemil.hgms.activity.MainActivity;
+import com.chaemil.hgms.fragment.ArchiveFragment;
 import com.chaemil.hgms.model.ArchiveItem;
 import com.chaemil.hgms.model.PhotoAlbum;
 import com.chaemil.hgms.model.Video;
@@ -54,6 +58,7 @@ public class ArchiveAdapter extends ArrayAdapter<ArchiveItem> {
             holder.date = (TextView) convertView.findViewById(R.id.date);
             holder.views = (TextView) convertView.findViewById(R.id.views);
             holder.type = (ImageView) convertView.findViewById(R.id.type_drawable);
+            holder.more = (ImageButton) convertView.findViewById(R.id.context_menu);
 
             convertView.setTag(holder);
         }
@@ -78,10 +83,14 @@ public class ArchiveAdapter extends ArrayAdapter<ArchiveItem> {
                 holder.name.setText(video.getName());
                 holder.date.setText(video.getDate());
                 holder.views.setText(video.getViews() + " " + context.getString(R.string.views));
+                holder.more.setVisibility(View.VISIBLE);
+                holder.more.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        contextDialog(video);
+                    }
+                });
                 Ion.with(context).load(video.getThumbFile()).intoImageView(holder.thumb);
-
-
-                int downloadStatus = Video.getDownloadStatus(video.getServerId());
 
                 break;
 
@@ -99,11 +108,65 @@ public class ArchiveAdapter extends ArrayAdapter<ArchiveItem> {
                 holder.name.setText(photoAlbum.getName());
                 holder.date.setText(photoAlbum.getDate());
                 holder.views.setText(context.getString(R.string.photo_album));
+                holder.more.setVisibility(View.GONE);
                 Ion.with(context).load(photoAlbum.getThumbs().getThumb512()).intoImageView(holder.thumb);
                 break;
         }
 
         return convertView;
+    }
+
+    private void contextDialog(final Video video) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        String[] menu;
+
+        if (Video.getDownloadStatus(video.getServerId()) == Video.NOT_DOWNLOADED) {
+            menu = new String[] {context.getString(R.string.download_audio),
+                                context.getString(R.string.stream_audio)};
+
+            builder.setItems(menu, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch(which) {
+                        case 0:
+                            downloadAudio(video);
+                            dialog.dismiss();
+                            break;
+                        case 1:
+                            mainActivity.playNewAudio(video, false);
+                            dialog.dismiss();
+                            break;
+                    }
+                }
+            });
+
+        }
+
+        if (Video.getDownloadStatus(video.getServerId()) == Video.DOWNLOADED) {
+            menu = new String[] {context.getString(R.string.play_downloaded_audio),
+                                context.getString(R.string.stream_audio)};
+
+            builder.setItems(menu, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    switch(which) {
+                        case 0:
+                            mainActivity.playNewAudio(video, true);
+                            dialog.dismiss();
+                            break;
+                        case 1:
+                            mainActivity.playNewAudio(video, false);
+                            dialog.dismiss();
+                            break;
+                    }
+                }
+            });
+
+        }
+
+
+        builder.create().show();
     }
 
     private void downloadAudio(Video video) {
@@ -130,6 +193,7 @@ public class ArchiveAdapter extends ArrayAdapter<ArchiveItem> {
         public TextView date;
         public TextView views;
         public ImageView type;
+        public ImageButton more;
 
     }
 
