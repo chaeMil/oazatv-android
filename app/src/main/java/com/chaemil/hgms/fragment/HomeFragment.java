0ba62f2cfec1f1ff07cf293fbe98add4f3c7d277
Michal Mlejnek <chaemil72@gmail.com>
2016-03-23 23:47:50 +0100

    add view2json project library add testing view to home fragment

--- app/src/main/java/com/chaemil/hgms/fragment/HomeFragment.java
+++ app/src/main/java/com/chaemil/hgms/fragment/HomeFragment.java
@@ -6,18 +6,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.avocarrot.json2view.DynamicView;
 import com.chaemil.hgms.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by chaemil on 2.12.15.
  */
 public class HomeFragment extends Fragment {
 
+    private JSONObject jsonObject;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.home_fragment, container, false);
 
+        try {
+            jsonObject = new JSONObject(getString(R.string.test_json_layout));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         getUI(rootView);
         setupUI();
@@ -31,6 +42,9 @@ public class HomeFragment extends Fragment {
     }
 
     private void getUI(ViewGroup rootView) {
-
+        View sampleView = DynamicView.createView(getActivity(), jsonObject, rootView);
+        if (sampleView != null) {
+            rootView.addView(sampleView);
+        }
     }
 }
