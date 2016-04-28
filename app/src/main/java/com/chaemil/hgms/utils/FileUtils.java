6cf389e29ceee1e8128db3f86c3f5b14c5a60f6e
Michal Mlejnek <chaemil72@gmail.com>
2016-04-28 17:55:06 +0200

    Utils tweak

--- app/src/main/java/com/chaemil/hgms/utils/FileUtils.java
+++ app/src/main/java/com/chaemil/hgms/utils/FileUtils.java
@@ -1,11 +1,7 @@
 package com.chaemil.hgms.utils;
 
-import android.content.Context;
-import android.os.Build;
 import android.os.Environment;
 import android.os.StatFs;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
 
 import java.io.File;
 import java.text.DecimalFormat;
@@ -17,6 +13,11 @@ public class FileUtils {
     public static final long SIZE_KB = 1024L;
     public static final long SIZE_MB = SIZE_KB * SIZE_KB;
 
+    public static boolean externalMemoryAvailable() {
+        return android.os.Environment.getExternalStorageState().equals(
+                android.os.Environment.MEDIA_MOUNTED);
+    }
+
     public static long getFreeSpace(){
         long availableSpace = -1L;
         StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
@@ -25,9 +26,15 @@ public class FileUtils {
     }
 
     public static long getExternalStorageSize() {
-        StatFs statFs = new StatFs(Environment.getExternalStorageDirectory().getAbsolutePath());
-        long blockSize = statFs.getBlockSize();
-        return statFs.getAvailableBlocks()*blockSize;
+        if (externalMemoryAvailable()) {
+            File path = Environment.getExternalStorageDirectory();
+            StatFs stat = new StatFs(path.getPath());
+            long blockSize = stat.getBlockSize();
+            long availableBlocks = stat.getAvailableBlocks();
+            return availableBlocks * blockSize;
+        } else {
+            return 0;
+        }
     }
 
     public static long getFolderSize(File f) {
