72a51db37fadd3096f24d5e20fddb5e873b1b474
Michal Mlejnek <chaemil72@gmail.com>
2016-09-07 22:17:15 +0200

    Put player to separate Fragment

---
+++ app/src/main/java/com/chaemil/hgms/fragment/video_player/QualitySwitchListener.java
@@ -0,0 +1,12 @@
+package com.chaemil.hgms.fragment.video_player;
+
+/**
+ * Created by chaemil on 7.9.16.
+ */
+public interface QualitySwitchListener {
+
+    void onHighQualitySet();
+
+    void onLowQualitySet();
+
+}
