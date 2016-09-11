8fdf0c9c0b752a1c6806f1a60caf572981375d2b
Michal Mlejnek <chaemil72@gmail.com>
2016-09-11 20:47:36 +0200

    Bump version Crash fixes

--- app/src/main/java/com/chaemil/hgms/OazaApp.java
+++ app/src/main/java/com/chaemil/hgms/OazaApp.java
@@ -23,7 +23,7 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
  */
 public class OazaApp extends SugarApp {
 
-    public static final boolean DEVELOPMENT = false;
+    public static final boolean DEVELOPMENT = true;
     public static final boolean TRACKER = true;
 
     private MainActivity mainActivity;
