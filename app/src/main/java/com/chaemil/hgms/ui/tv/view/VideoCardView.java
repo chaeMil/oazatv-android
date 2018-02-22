4c965e9d6dec4f92e472e34e275dc12731631d3c
Michal Mlejnek <chaemil72@gmail.com>
2018-02-22 14:50:16 +0100

    Add video time, CC and language info to tv card layout

--- app/src/main/java/com/chaemil/hgms/ui/tv/view/VideoCardView.java
+++ app/src/main/java/com/chaemil/hgms/ui/tv/view/VideoCardView.java
@@ -2,6 +2,7 @@ package com.chaemil.hgms.ui.tv.view;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +10,7 @@ import com.chaemil.hgms.R;
 import com.chaemil.hgms.databinding.TvVideoCardViewBinding;
 import com.chaemil.hgms.model.Video;
 import com.chaemil.hgms.ui.tv.view.BindableCardView;
+import com.chaemil.hgms.utils.StringUtils;
 import com.koushikdutta.ion.Ion;
 
 import butterknife.BindView;
@@ -26,6 +28,15 @@ public class VideoCardView extends BindableCardView<Video> {
     @BindView(R.id.name)
     TextView name;
 
+    @BindView(R.id.cc)
+    TextView cc;
+
+    @BindView(R.id.language)
+    TextView language;
+
+    @BindView(R.id.video_time)
+    TextView videoTime;
+
     private final Context context;
 
     public VideoCardView(Context context) {
@@ -40,6 +51,10 @@ public class VideoCardView extends BindableCardView<Video> {
                 .load(video.getThumbFile())
                 .intoImageView(thumb);
         name.setText(video.getName());
+        cc.setVisibility(video.getSubtitlesFile() != null ? View.VISIBLE : View.GONE);
+        videoTime.setText(StringUtils.getDurationString(video.getDuration()));
+        language.setVisibility(video.getVideoLanguage(context) != null ? View.VISIBLE : View.GONE);
+        language.setText(video.getVideoLanguage(context));
     }
 
     @Override
