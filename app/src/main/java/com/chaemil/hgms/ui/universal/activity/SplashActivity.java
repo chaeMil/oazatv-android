b78a79b15b681b169f46cf5dc1550e8159a41c3c
Michal Mlejnek <chaemil72@gmail.com>
2018-02-22 14:44:53 +0100

    Refactor

--- app/src/main/java/com/chaemil/hgms/ui/universal/activity/SplashActivity.java
+++ app/src/main/java/com/chaemil/hgms/ui/universal/activity/SplashActivity.java
@@ -5,9 +5,8 @@ import android.content.Intent;
 import com.chaemil.hgms.OazaApp;
 import com.chaemil.hgms.R;
 import com.chaemil.hgms.ui.mobile.activity.BaseActivity;
-import com.chaemil.hgms.ui.mobile.activity.MainActivity;
 import com.chaemil.hgms.ui.mobile.activity.TutorialActivity;
-import com.chaemil.hgms.ui.tv.activity.TvMainActivity;
+import com.chaemil.hgms.ui.tv.activity.MainActivity;
 import com.chaemil.hgms.utils.FileUtils;
 import com.chaemil.hgms.utils.SharedPrefUtils;
 import com.chaemil.hgms.utils.TVUtils;
@@ -42,7 +41,7 @@ public class SplashActivity extends BaseActivity {
 
     public void startMainActivity() {
         delay(() -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            Intent intent = new Intent(SplashActivity.this, com.chaemil.hgms.ui.mobile.activity.MainActivity.class);
             startActivity(intent);
             finish();
             overridePendingTransition(0, 0);
@@ -51,7 +50,7 @@ public class SplashActivity extends BaseActivity {
 
     public void startTvActivity() {
         delay(() -> {
-            Intent intent = new Intent(SplashActivity.this, TvMainActivity.class);
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             startActivity(intent);
             finish();
             overridePendingTransition(0, 0);
